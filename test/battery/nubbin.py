0deea8df347968547f0576fac3d461d3d4b59907
JeremyPFrank <111478725+JeremyPFrank@users.noreply.github.com>
2023-07-21 14:58:20 -0700

    Update nubbin.py

--- test/battery/nubbin.py
+++ test/battery/nubbin.py
@@ -1,1 +1,14 @@
+from Battery.NubbinBattery import NubbinBattery
+import unittest
 
+class TestNubbinBattery(unittest.TestCase):
+  def does_need_service(self):
+    current_date = date.fromisoformat("2023-07-21")
+    last_service_date = date.fromisoformat("2005-02-04")
+    battery=NubbinBattery(current_date,last_service_date)
+    self.assertTrue(engine.needs_service())
+  def not_need_service(self):
+    current_date = date.fromisoformat("2023-07-21")
+    last_service_date = date.fromisoformat("2023-05-20")
+    battery=NubbinBattery(current_date,last_service_date)
+    self.assertTrue(engine.needs_service())
