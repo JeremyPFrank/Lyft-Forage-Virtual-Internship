a2e3082d49b85e615b0d652c66b2c477d0b70b0c
JeremyPFrank <111478725+JeremyPFrank@users.noreply.github.com>
2023-07-21 14:53:47 -0700

    Update sternman.py

--- test/engine/sternman.py
+++ test/engine/sternman.py
@@ -1,1 +1,10 @@
+from engine.sternman_engine import SternmanEngine
+import unittest
 
+class TestSternmanEngine(unittest.TestCase):
+  def does_need_service(self):
+    engine=SternmanEngine(True)
+    self.assertTrue(engine.needs_service())
+  def not_need_service(self):
+    engine=SternmanEngine(False)
+    self.assertTrue(engine.needs_service())
