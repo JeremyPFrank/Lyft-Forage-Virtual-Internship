2c0dd53da2c77db4bc26b791bd7bedf9b8754cc0
JeremyPFrank <111478725+JeremyPFrank@users.noreply.github.com>
2023-07-21 14:54:34 -0700

    Update willoughby.py

--- test/engine/willoughby.py
+++ test/engine/willoughby.py
@@ -1,1 +1,10 @@
+from engine.willoughby_engine import WilloughbyEngine
+import unittest
 
+class TestWilloughbyEngine(unittest.TestCase):
+  def does_need_service(self):
+    engine=WilloughbyEngine(60001,0)
+    self.assertTrue(engine.needs_service())
+  def not_need_service(self):
+    engine=WilloughbyEngine(59000,0)
+    self.assertTrue(engine.needs_service())
