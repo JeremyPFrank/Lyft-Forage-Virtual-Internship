65ae2e7e171c5dc479fa7e11f4ee10ed4d78a449
JeremyPFrank <111478725+JeremyPFrank@users.noreply.github.com>
2023-07-21 12:21:55 -0700

    Update NubbinBattery.py

--- Battery/NubbinBattery.py
+++ Battery/NubbinBattery.py
@@ -1,1 +1,10 @@
+from Battery.battery import Battery
 
+class NubbinBattery(Battery):
+  def _init_(self, last_service_date, current_date):
+    self.last_service_date = last_service_date
+    self.current_date = current_date
+  def needs_service(self):
+    if current_date - last_service_date >= 4:
+      return True
+    return False
