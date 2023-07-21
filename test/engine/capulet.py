2b4e096071ee1969ae1eaf2f1a03aed58bc54c91
JeremyPFrank <111478725+JeremyPFrank@users.noreply.github.com>
2023-07-21 14:52:40 -0700

    Update capulet.py

--- test/engine/capulet.py
+++ test/engine/capulet.py
@@ -1,1 +1,10 @@
+from engine.capulet_engine import CapuletEngine
+import unittest
 
+class TestCapuletEngine(unittest.TestCase):
+  def does_need_service(self):
+    engine=CapuletEngine(30001,0)
+    self.assertTrue(engine.needs_service())
+  def not_need_service(self):
+    engine=CapuletEngine(29000,0)
+    self.assertTrue(engine.needs_service())
