438756d2b6a08212fde3f90ea8c7a98dc2ed219f
JeremyPFrank <111478725+JeremyPFrank@users.noreply.github.com>
2023-07-21 12:29:50 -0700

    Update carfactory.py

--- carfactory.py
+++ carfactory.py
@@ -1,1 +1,10 @@
+from abc import ABC
 
+class Carfactory(ABC):
+
+  def create_calliope(current_date: date, last_service_date: date, current_mileage: int, last_service_mileage: int): 
+    
+  def create_glissade(current_date: date, last_service_date: date, current_mileage: int, last_service_mileage: int): 
+  def create_palindrome(current_date: date, last_service_date: date, warning_light_on: bool): 
+  def create_rorschach(current_date: date, last_service_date: date, current_mileage: int, last_service_mileage: int):
+  def create_thovex(current_date: date, last_service_date: date, current_mileage: int, last_service_mileage: int):
