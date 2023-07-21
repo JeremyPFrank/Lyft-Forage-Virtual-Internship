21f4b761e37bab938132759157fa6d48a42dca25
JeremyPFrank <111478725+JeremyPFrank@users.noreply.github.com>
2023-07-21 12:31:28 -0700

    Update carfactory.py

--- carfactory.py
+++ carfactory.py
@@ -3,7 +3,7 @@ from abc import ABC
 class Carfactory(ABC):
 
   def create_calliope(current_date: date, last_service_date: date, current_mileage: int, last_service_mileage: int): 
-    
+    engine = Capulet_Engine(
   def create_glissade(current_date: date, last_service_date: date, current_mileage: int, last_service_mileage: int): 
   def create_palindrome(current_date: date, last_service_date: date, warning_light_on: bool): 
   def create_rorschach(current_date: date, last_service_date: date, current_mileage: int, last_service_mileage: int):
