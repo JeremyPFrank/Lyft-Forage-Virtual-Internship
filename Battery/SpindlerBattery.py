8ea3b3d1dbc78f1c7e0f36e2dc138abb23cd050a
JeremyPFrank <111478725+JeremyPFrank@users.noreply.github.com>
2023-07-21 12:25:28 -0700

    Update SpindlerBattery.py

--- Battery/SpindlerBattery.py
+++ Battery/SpindlerBattery.py
@@ -5,6 +5,7 @@ class SpindlerBattery(Battery):
     self.last_service_date = last_service_date
     self.current_date = current_date
   def needs_service(self):
-    if current_date - last_service_date >= 2:
+    service_date = last_service_date.replace(year=last_service_date.year + 2)
+    if current_date >= service_date
       return True
     return False
