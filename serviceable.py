b365aaad266fa665b5a84e75e09c3ff763f7c5bd
JeremyPFrank <111478725+JeremyPFrank@users.noreply.github.com>
2023-07-21 12:11:49 -0700

    Update serviceable.py

--- serviceable.py
+++ serviceable.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstract class
 
-def needs_service(car):
-  if car.needs_service is True:
-    return True
-  return False
+class Serviceable(ABC):
+  @abstractmethod
+  def needs_service(self):
+    pass
