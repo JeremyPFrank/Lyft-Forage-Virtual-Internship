b48e50e2dcf16188177f5b5c712fe1708be62ff9
JeremyPFrank <111478725+JeremyPFrank@users.noreply.github.com>
2023-07-21 20:31:08 -0700

    Update octoprime_tire.py

--- test/tire/octoprime_tire.py
+++ test/tire/octoprime_tire.py
@@ -3,8 +3,8 @@ import unittest
 
 class TestOctoprimeTire(unittest.TestCase):
   def does_need_service(self):
-    tire=OctoprimeTire((0.9,0.8,0.89,0.91))
+    tire=OctoprimeTire([0.9,0.8,0.89,0.91])
     self.assertTrue(engine.needs_service())
   def not_need_service(self):
-    tire=OctoprimeTire((0.1,0.5,0.4,0.5))
+    tire=OctoprimeTire([0.1,0.5,0.4,0.5])
     self.assertTrue(engine.needs_service())
