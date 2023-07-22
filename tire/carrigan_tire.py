ca490609592348f7fb6995782f026365b0b73334
JeremyPFrank <111478725+JeremyPFrank@users.noreply.github.com>
2023-07-21 20:16:59 -0700

    Update carrigan_tire.py

--- tire/carrigan_tire.py
+++ tire/carrigan_tire.py
@@ -1,1 +1,11 @@
+from tire.tire import Tire
 
+class CarriganTire(Tire):
+    def __init__(self, tire_wear):
+        self.tire_wear = tire_wear
+
+    def needs_service(self):
+      for tire in tire_wear:
+        if tire>=0.9:
+          return True
+      return False
