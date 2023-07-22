8e21d67c00d96ae003ce35116eeebcc64787643c
JeremyPFrank <111478725+JeremyPFrank@users.noreply.github.com>
2023-07-21 20:30:51 -0700

    Update Carrigan_tire.py

--- test/tire/Carrigan_tire.py
+++ test/tire/Carrigan_tire.py
@@ -3,8 +3,8 @@ import unittest
 
 class TestCarriganTire(unittest.TestCase):
   def does_need_service(self):
-    tire=CarriganTire((0.1,0.5,0.89,0.91))
+    tire=CarriganTire([0.1,0.5,0.89,0.91])
     self.assertTrue(engine.needs_service())
   def not_need_service(self):
-    tire=CarriganTire((0.1,0.5,0.89,0.88))
+    tire=CarriganTire([0.1,0.5,0.89,0.88])
     self.assertTrue(engine.needs_service())
