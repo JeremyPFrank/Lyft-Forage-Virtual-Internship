671f15ec339a7997b203386279858412b46f9042
JeremyPFrank <111478725+JeremyPFrank@users.noreply.github.com>
2023-07-21 20:22:30 -0700

    Update carfactory.py

--- carfactory.py
+++ carfactory.py
@@ -9,23 +9,23 @@ from car import Car
 
 class CarFactory:
 
-  def create_calliope(current_date: date, last_service_date: date, current_mileage: int, last_service_mileage: int): 
+  def create_calliope(current_date, last_service_date, current_mileage, last_service_mileage, tire): 
     engine = CapuletEngine(current_mileage,last_service_mileage)
     battery = SpindlerBattery(current_date,last_service_date)
-    return Car(engine,battery)
-  def create_glissade(current_date: date, last_service_date: date, current_mileage: int, last_service_mileage: int): 
+    return Car(engine,battery,tire)
+  def create_glissade(current_date, last_service_date, current_mileage, last_service_mileage,tire): 
     engine = WilloughbyEngine(current_mileage,last_service_mileage)
     battery = SpindlerBattery(current_date,last_service_date)
-    return Car(engine,battery)
-  def create_palindrome(current_date: date, last_service_date: date, warning_light_on: bool): 
+    return Car(engine,battery,tire)
+  def create_palindrome(current_date, last_service_date, warning_light_on, tire): 
     engine = SternmanEngine(warning_light_on)
     battery = SpindlerBattery(current_date,last_service_date)
-    return Car(engine,battery)
-  def create_rorschach(current_date: date, last_service_date: date, current_mileage: int, last_service_mileage: int):
+    rreturn Car(engine,battery,tire)
+  def create_rorschach(current_date, last_service_date, current_mileage, last_service_mileage,tire):
     engine = WilloughbyEngine(current_mileage,last_service_mileage)
     battery = NubbinBattery(current_date,last_service_date)
-    return Car(engine,battery)
-  def create_thovex(current_date: date, last_service_date: date, current_mileage: int, last_service_mileage: int):
+    return Car(engine,battery,tire)
+  def create_thovex(current_date, last_service_date, current_mileage, last_service_mileage,tire):
     engine = CapuletEngine(current_mileage,last_service_mileage)
     battery = NubbinBattery(current_date,last_service_date)
-    return Car(engine,battery)
+    return Car(engine,battery,tire)
